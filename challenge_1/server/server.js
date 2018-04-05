42b1f14b4f56661afcee0e1fb71fa786fe05cca3
j-ornelas <johnnyoh1988@gmail.com>
2018-04-04 23:20:52 -0700

    Added axios call to API

--- challenge_1/server/server.js
+++ challenge_1/server/server.js
@@ -4,9 +4,27 @@ const port = 4200;
 const title =  `CryptoChart`;
 const path = require('path');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
+app.use('/', express.static(path.join(__dirname, '../client/public')));
+app.use('/date', express.static(path.join(__dirname, '../client/public')));
 
-app.use('/', express.static(path.join(__dirname, '../client/public')))
+app.get('/info', function(req, res){
+  console.log(req.query.url)
+  axios.get(req.query.url)
+    .then(function(response){
+      res.send(response.data.bpi)
+    })
+    .catch(function(error){
+      console.log(error)
+    })
+});
+
+// app.get('/date', function(req, res){
+//   console.log(req.body)
+//   res.send(path.join(__dirname, '../client/public/index.html'))
+// })
+
 
 
 app.listen(port, () => console.log(`${title} listening on port ${port}!`))
